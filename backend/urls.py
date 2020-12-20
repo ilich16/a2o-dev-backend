30c021ce9de08fe715cfa483e63d819462ced8d5
unknown <diego.severiche89@gmail.com>
2020-12-20 19:12:27 -0400

    Add the url for the API

--- backend/urls.py
+++ backend/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('api/v1/', include('backend.urls')),
+    path('api/v1/', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
