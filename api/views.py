5efbf19234a59b0111e3ce8717d156b6fc34cece
unknown <diego.severiche89@gmail.com>
2020-12-20 19:25:34 -0400

    Add code to file

--- api/views.py
+++ api/views.py
@@ -3,3 +3,14 @@ from django.http import JsonResponse
 from api.models.first_problem import FirstProblem
 
 # Create your views here.
+def first_problem(request):
+    # Get the url query
+    user_input = request.GET.get('input', '')
+    # Create a new object to work
+    first_problem = FirstProblem(user_input)
+    # Get the answer of the problem
+    result = first_problem.get_answer()
+    # Create a JsonResponse object for return it
+    response = JsonResponse({'result': result})
+
+    return response
