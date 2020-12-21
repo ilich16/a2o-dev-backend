87f4ccb487da44bbf2c03f66eced270b6e04a820
unknown <diego.severiche89@gmail.com>
2020-12-21 00:18:29 -0400

    Add a view for the third problem

--- api/views.py
+++ api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from api.models.first_problem import FirstProblem
 from api.models.second_problem import SecondProblem
+from api.models.third_problem import ThirdProblem
 
 # Create your views here.
 def first_problem(request):
@@ -23,6 +24,18 @@ def second_problem(request):
     second_problem = SecondProblem(user_input)
     # Get the answer of the problem
     result = second_problem.get_answer()
+    # Create a JsonResponse object for return it
+    response = JsonResponse({'result': result})
+
+    return response
+
+def third_problem(request):
+    # Get the url query
+    user_input = request.GET.get('input', '')
+    # Create a new object to work
+    third_problem = ThirdProblem(user_input)
+    # Get the answer of the problem
+    result = third_problem.get_answer()
     # Create a JsonResponse object for return it
     response = JsonResponse({'result': result})
 
