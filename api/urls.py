502fa92c463837fe853faaf2ef159b30a38a4051
unknown <diego.severiche89@gmail.com>
2020-12-21 00:19:00 -0400

    Add an url for the third problem

--- api/urls.py
+++ api/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
   path('problem-1/', views.first_problem, name='first_problem'),
   path('problem-2/', views.second_problem, name='second_problem'),
+  path('problem-3/', views.third_problem, name='third_problem'),
 ]
